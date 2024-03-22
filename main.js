8d9eae567e321c317d93c2c2379a20d24d0c5103
pauloh <106337186+Pauloh1CDP@users.noreply.github.com>
2024-03-22 11:19:25 -0300

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-const botoes = document.querySelectorAll(".botao");
-for(let i= 0; i< botoes.lenght; i++){
-    botoes[i].onclick = function(){
-        for(j=0; j<botoes.lenght; j++){
-            botoes[j].claslist.remove("ativo");
-        }
-        botoes[i].classlist.add("ativo");
-    }
-}
